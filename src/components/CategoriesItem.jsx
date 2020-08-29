673b9663a0fffcafe5503260e5c10b521a95ed75
bananchik228 <xzelik@gmail.com>
2020-08-29 22:25:18 +0300

    Поправил выделение категории, оформил формил форму, поправил общие стили

--- src/components/CategoriesItem.jsx
+++ src/components/CategoriesItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default ({name, queryCategory}) => {
-    const select = name === queryCategory ? 'categories__item_select' : null
+    const select = name === queryCategory ? 'categories__item_select' : ''
 
     return (
         <div className={`categories__item ${select}`}>
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,3 +1,102 @@
+import React from 'react'
+
+import '@styles/Order.scss'
+
 export default () => {
-    return 'This is Order!'
+    return (
+        <form className="order">
+            <div className="order__message order__message_info">
+                <b>Ваша корзина пуста.</b> Перед отправкой заказа добавьте в корзину хотя бы один товар.
+            </div>
+
+            <div className="order__item">
+                <div className="order__name">
+                    <label htmlFor="input-name">
+                        <b>Ваше имя *</b>
+                    </label>
+                </div>
+                <div className="order__input">
+                    <input type="text" placeholder="Ваше имя" id="input-name" />
+                </div>
+            </div>
+
+            <div className="order__item">
+                <div className="order__name">
+                    <label htmlFor="input-email">
+                        <b>Email *</b>
+                    </label>
+                </div>
+                <div className="order__input">
+                    <input type="text" placeholder="Email" id="input-email" />
+                </div>
+            </div>
+
+            <div className="order__item">
+                <div className="order__name">
+                    <label htmlFor="input-phone">
+                        <b>Телефон</b>
+                    </label>
+                </div>
+                <div className="order__input">
+                    <input type="text" placeholder="Телефон" id="input-phone" />
+                </div>
+            </div>
+
+            <div className="order__item">
+                <div className="order__name" />
+                <div className="order__input">
+                    <div className="order__message order__message_warning">
+                        <b>Важно!</b> Укажите номер, что бы мы связались с вами раньше.
+                    </div>
+                </div>
+            </div>
+
+            <div className="order__item">
+                <div className="order__name">
+                    <label htmlFor="input-phone">
+                        <b>Способ доставки</b>
+                    </label>
+                </div>
+                <div className="order__input">
+                    <p>
+                        <input name="delivery" type="radio" value="pickup" /> 
+                        Самовывоз
+                    </p>
+                    <p>
+                        <input name="delivery" type="radio" value="postOffice" /> 
+                        Почта России (300 руб.)
+                    </p>
+                </div>
+            </div>
+
+            <div className="order__item">
+                <div className="order__name">
+                    <label htmlFor="input-address">
+                        <b>Адрес доставки</b>
+                    </label>
+                </div>
+                <div className="order__input">
+                    <input type="text" placeholder="Адрес доставки" id="input-address" />
+                </div>
+            </div>
+
+            <div className="order__item">
+                <div className="order__name">
+                    <label htmlFor="input-message">
+                        <b>Сообщение</b>
+                    </label>
+                </div>
+                <div className="order__input">
+                    <textarea type="text" placeholder="Доп. информация" id="input-message" />
+                </div>
+            </div>
+
+            <div className="order__item">
+                <div className="order__name" />
+                <div className="order__input">
+                    <button className="order__submit">Отправить заказ</button>
+                </div>
+            </div>
+        </form>
+    )
 }
